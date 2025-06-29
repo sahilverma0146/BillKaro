83665e4a016d524a87796f1d8ca97b9bc75dbe67
Sahil <sahilverma22146@gmail.com>
2025-06-29 12:01:15 +0530

    dev3

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 
@@ -12,40 +12,42 @@ app.use(cors());
 // body parser
 app.use(express.json());
 
-
-
 const router = express.Router();
-const ItemRouter = require('./Controller/ItemController');
-const CustomerRouter = require('./Controller/CustomerController')
-const AuthController = require('./Controller/AuthController');
-const authMiddleware = require('./Controller/authMiddleware');
+const ItemRouter = require("./Controller/ItemController");
+const CustomerRouter = require("./Controller/CustomerController");
+const AuthController = require("./Controller/AuthController");
+const authMiddleware = require("./Controller/authMiddleware");
+
+app.use("/api", router);
+
+app.use("/", (req, res) => {
+  res.send({
+    activeStatus:true,
+    error : false,
+  })
+});
 
 
-app.use('/api', router);
+router.post("/NewItem", authMiddleware, ItemRouter.AddItem);
+router.get("/GetItem", authMiddleware, ItemRouter.GetItem);
 
-router.post('/NewItem', authMiddleware, ItemRouter.AddItem);
-router.get('/GetItem', authMiddleware, ItemRouter.GetItem);
+router.post("/AddCustomer", authMiddleware, CustomerRouter.ProduceBill);
+router.get("/getCustomer/:_id", authMiddleware, CustomerRouter.FetchCustomer);
+router.post("/saveBill", authMiddleware, CustomerRouter.SaveBill);
+router.get("/getBill", authMiddleware, CustomerRouter.getBills);
 
-router.post('/AddCustomer', authMiddleware, CustomerRouter.ProduceBill);
-router.get('/getCustomer/:_id', authMiddleware, CustomerRouter.FetchCustomer);
-router.post('/saveBill', authMiddleware, CustomerRouter.SaveBill);
-router.get('/getBill', authMiddleware, CustomerRouter.getBills);
-
-app.post('/api/auth/register', AuthController.register);
-app.post('/api/auth/login', AuthController.login);
+app.post("/api/auth/register", AuthController.register);
+app.post("/api/auth/login", AuthController.login);
 
 // connection
-main().catch(err => console.log(err));
+main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(String(process.env.URL));
-  console.log("your connection is successful")
-
+  console.log("your connection is successful");
 }
-
 
 //port details
 
-app.listen(process.env.PORT,()=>{
-    console.log("You are on  " +`${process.env.PORT}`)
-
-})
+app.listen(process.env.PORT, () => {
+  console.log("You are on  " + `${process.env.PORT}`);
+});
